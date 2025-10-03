2d80631ecd5999b069cf99259b8500d82ad06ae5
Vinícius Silva Rodrigues <vini.bnu.rodrigues@gmail.com>
2025-10-03 11:33:41 -0300

    Create Atividade2.java

---
+++ Laços-de-Repetição/While/Atividade2.java
@@ -0,0 +1,16 @@
+public class exdois {
+    public static void main(String[] args) {
+        int cont=0;
+
+        while(cont<=50);{
+
+        if(cont % 2 == 0)
+        {
+            System.out.println(cont +", ");
+        }
+     cont++;
+    }
+
+    }
+    
+}
