ff5c3cb08b5422939568c42ec2df36595a422920
Vinícius Silva Rodrigues <vini.bnu.rodrigues@gmail.com>
2025-10-03 11:25:39 -0300

    Create Atividade2.java

---
+++ Laços-de-Repetição/For/Atividade2.java
@@ -0,0 +1,11 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        
+        int contador = 0;
+        
+        for(contador = 10; contador > 1; contador--){
+            
+            System.out.println(contador);
+        }
+    }
+}
