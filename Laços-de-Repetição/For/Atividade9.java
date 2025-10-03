2158a85e4ad5215fa1aba6dc265fd6d04d5c61fa
Vinícius Silva Rodrigues <vini.bnu.rodrigues@gmail.com>
2025-10-03 11:29:38 -0300

    Create Atividade9.java

---
+++ Laços-de-Repetição/For/Atividade9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Atividade9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n ;
+
+        System.out.println("informe um valor: ");
+        n = sc.nextInt();
+
+        for(int contador = 0; contador <= n; contador ++){
+            System.out.println(contador);
+        }
+        
+    }
+}
